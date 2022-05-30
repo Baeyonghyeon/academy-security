52154dc10779f795dd3bf74851d95aa9d3bd1359
yonghyeon <qodydgus97@gmail.com>
2022-05-30 16:08:22 +0900

    a

--- src/main/java/com/nhnacademy/security/entitiy/Member.java
+++ src/main/java/com/nhnacademy/security/entitiy/Member.java
@@ -2,17 +2,23 @@ package com.nhnacademy.security.entitiy;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Setter
 @Getter
 @Entity
 @Table(name = "Members")
-public class Member {
+public class Member implements UserDetails {
 
     @Id
     @Column(name = "member_id")
@@ -23,4 +29,44 @@ public class Member {
 
     @Column
     String pwd;
+
+    @Column
+    String auth;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> auth = new ArrayList<GrantedAuthority>();
+        auth.add(new SimpleGrantedAuthority(this.auth));
+        return auth;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.pwd;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.id;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
--- src/main/java/com/nhnacademy/security/repository/LoginRepository.java
+++ src/main/java/com/nhnacademy/security/repository/LoginRepository.java
@@ -4,4 +4,5 @@ import com.nhnacademy.security.entitiy.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoginRepository extends JpaRepository<Member, String> {
+
 }
